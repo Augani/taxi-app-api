539daa221089f11e70350d63e8e1e461b384ab85
Augustus Otu <Augani7@gmail.com>
2019-07-03 15:36:14 +0000

    changed

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
-const express = require('express');
-const http = require('http')
-const socketio = require('socket.io');
+var express = require('express');
+var app = express();
+const PORt = 3232 || process.env.PORT;
+var server = app.listen(PORt);
+var io = require('socket.io').listen(server);
 
-const app = express();
-const server = http.Server(app);
-const websocket = socketio(server);
+
 const mongoose = require('mongoose');
- const connectionString = 'mongodb+srv://nii:0277427898@taxi-lvqnv.mongodb.net/test?retryWrites=true&w=majority'
-// const connectionString = 'mongodb://localhost:27017/taxi';
+  const connectionString = 'mongodb+srv://nii:0277427898@taxi-lvqnv.mongodb.net/test?retryWrites=true&w=majority'
+//  const connectionString = 'mongodb://localhost:27017/taxi';
 const connector = mongoose.connect(connectionString,{useNewUrlParser: true});
 connector.then(r=>{
   console.log('connected');
@@ -23,14 +23,13 @@ const login = require('./api/auth/login');
 
 app.use('/api', login);
 
-const PORt = 3232 || process.env.PORT;
-server.listen(PORt, function(){
-  console.log('listening on *:3000');
-});
 
-console.log(websocket)
+// server.listen(PORt, function(){
+//   console.log('listening on *:3000');
+// });
+
 
 // The event will be called when a client is connected.
-websocket.on('connection', (socket) => {
+io.on('connection', (socket) => {
   console.log('A client just joined on', socket.id);
 });
